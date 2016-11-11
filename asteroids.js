acefc904d0a8d4a6fc88647acc796b8d6c390a2a
Greg Gorlen <gsgorlen@gmail.com>
2016-11-11 01:09:55 -0800

    revert spaceship image extension to .gif

--- asteroids.js
+++ asteroids.js
@@ -2,7 +2,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.image('spaceship', 'spaceship.png');
+    game.load.image('spaceship', 'spaceship.gif');
 }
 
 function create() {
