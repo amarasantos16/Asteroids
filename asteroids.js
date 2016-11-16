61709b5d3b5ccb9dd455f3e07b2f7d712558f155
amarasantos16 <amsantos@s.sfusd.edu>
2016-11-15 17:32:34 -0800

    got keyboard input

--- asteroids.js
+++ asteroids.js
@@ -1,13 +1,33 @@
-
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var kbd;
+var spaceship;
 
 function preload() {
     game.load.image('spaceship', 'spaceship.gif');
+    kbd = game.input.keyboard.createCursorKeys();
 }
 
 function create() {
-    game.add.sprite(0, 0, 'spaceship');
+
+    //  Our player ship
+    spaceship = game.add.sprite(300, 300, 'spaceship');
+    spaceship.anchor.set(0.5);
+
+    //  and its physics settings
+    game.physics.enable(spaceship, Phaser.Physics.ARCADE);
+
 }
 
+
 function update() {
+ 
+    if (kbd.up.isDown)  // isDown means key was pressed
+    {
+        game.physics.arcade.accelerationFromRotation(spaceship.rotation, 80, spaceship.body.acceleration);
+    }
+    else
+    {
+        spaceship.body.acceleration.set(0);
+    }
+
 }
